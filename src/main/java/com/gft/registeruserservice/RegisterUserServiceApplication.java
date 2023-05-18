57fe96675ca946b92492891eb4ea4759b1d2a20e
Eli <elileite9@gmail.com>
2023-05-18 16:29:32 -0300

    commit inicial do projeto.

---
+++ src/main/java/com/gft/registeruserservice/RegisterUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gft.registeruserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gft/registeruserservice/RegisterUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.registeruserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
