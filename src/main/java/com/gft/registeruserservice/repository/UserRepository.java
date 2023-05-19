3cbfb7894a2b38a0e70140dba69b63b5bc807c94
Eli <elileite9@gmail.com>
2023-05-19 18:41:11 -0300

    criando a interface do repositorio.

---
+++ src/main/java/com/gft/registeruserservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gft.registeruserservice.repository;
+
+import com.gft.registeruserservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends ListCrudRepository<User, Long>, JpaRepository<User, Long> {
+}
