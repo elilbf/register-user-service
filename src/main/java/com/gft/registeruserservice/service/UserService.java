ca955cad16cd5a153edd934c095ae254b9b5df33
Eli <elileite9@gmail.com>
2023-05-19 14:22:00 -0300

    criando a camada de servico de usuario.

---
+++ src/main/java/com/gft/registeruserservice/service/UserService.java
@@ -0,0 +1,30 @@
+package com.gft.registeruserservice.service;
+
+import com.gft.registeruserservice.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    public void createUser(User user){
+
+    }
+
+    public List<User> findAllUsers(){
+        return null;
+    }
+
+    public User findUserById(long id){
+        return null;
+    }
+
+    public void deleteUserById(long id){
+    }
+
+    public void deleteAllUsers(){
+
+    }
+
+}
